8c26a46c03f35dd428fc5758d8b35474003ce7ea
kschmuck <klemens.schmuck@tugraz.at>
2018-01-08 12:56:18 +0100

    Image: delete old force calculation, move IDPP class to own file

--- NEB.py
+++ NEB.py
@@ -1,5 +1,6 @@
-# from pes import energy, gradient
-from ethane_PES import energy_and_gradient
+from pes import energy, gradient
+from pes import energy_b, grad
+# from ethane_PES import energy_and_gradient
 # from HNCPES import energy_and_gradient
 # from Au55Ni import energy_and_gradient
 import numpy as np
@@ -10,7 +11,6 @@ import xyz_file_writer as xyz_writer
 from optimize import SteepestDecent, Verlete, Fire, ConjuageGradient, BFGS
 import time
 from data_reader_writer import Reader, Writer
-
 
 ''' creation of the image positions --> class images '''
 def create_images(product, reactant, number_of_images):
@@ -38,13 +38,14 @@ def set_images(positions):
     return images
 
 
-def center_geometry(positions):
+def center_geometry(positions, number_of_coordinates=3):
     # cartesian
     # atoms in row and coordiantes in columns x, y, z
-    mean_value = np.zeros([len(positions), 3])
+    mean_value = np.zeros([len(positions), number_of_coordinates])
     for kk in range(0, len(positions)):
         pos = positions[kk, :, :]
-        mean_value[kk, :] = np.array([np.mean(pos[:, 0]), np.mean(pos[:, 1]), np.mean(pos[:, 2])])
+        for cc in range(0, number_of_coordinates):
+            mean_value[kk, cc] = np.mean(pos[:, cc])
     return mean_value
 
 
@@ -114,103 +115,12 @@ def spring_force(img_0, img_1, img_2):
 def energy_gradient_surface(positions):
     # here place the fucntion for the evaluation of the gradient and energy
     # position = img.position
-    energy_surface, gradient_surface = energy_and_gradient(positions)
+    # energy_surface, gradient_surface = 0#energy_and_gradient(positions)
+    energy_surface = energy_b(positions)
+    gradient_surface = grad(positions)
     return energy_surface, gradient_surface
 
-# def energy_gradient_surface(*args):
-#     # here place the fucntion for the evaluation of the gradient and energy
-#     # position = img.position
-#     energy_surface, gradient_surface = energy_and_gradient('dummy')
-#     return energy_surface, gradient_surface
 
-
-class IDPP:
-    def __init__(self, images, coordinate_system='cartesian'):
-        if coordinate_system == 'cartesian':
-            self.num_atoms = int(len(images[0].position)/3)
-            self.number_of_coordinates = 3
-
-        d_product = self.calc_distance(images[0].position)
-        d_reactant = self.calc_distance(images[-1].position)
-
-        for kk in range(0, len(images)):
-            images[kk].d_ij_k = d_product + (1.0*kk)/(len(images)-1) * (d_reactant - d_product)
-
-    def calc_distance(self, position):
-        distance = np.zeros([self.num_atoms, self.num_atoms])
-
-        def distance_function(atom_i, atom_j):
-            return np.linalg.norm(atom_i-atom_j)
-
-        for ii in range(0, self.num_atoms):
-            index_ii = np.arange(ii*3, (ii+1)*3)
-            for jj in range(ii+1, self.num_atoms):
-                index_jj = np.arange(jj * 3, (jj + 1) * 3)
-                distance[ii, jj] = distance_function(position[index_ii], position[index_jj])
-        return distance
-
-    def calc_forces_numerical(self, positions, d_ij_k):
-        # weighting with 1/d**4
-        def func_derivative(dx_i, dx_j, atom_i, atom_j, value):
-            # dx_i derivative of the coordinate
-            distance = np.linalg.norm(atom_i-atom_j)
-            derivative = -((4 * (dx_i - dx_j) * (value - distance) ** 2) / distance ** (6./2.)) - \
-                          ((2 * (dx_i - dx_j) * (value - distance)) / distance ** (5./2.))
-            return derivative
-        force_ixyz = np.zeros(np.shape(positions))
-
-        # ij = 0
-        # for ii in range(0, self.num_atoms):
-        #     index_ii = np.arange(ii * 3, (ii + 1) * 3)
-        #     atom_ii = positions[index_ii]
-        #     for cc in range(0, 3):
-        #         force = 0.0
-        #         for jj in range(ii+1, self.num_atoms): #ii+1
-        #             # if ii == jj:
-        #             #     continue
-        #             index_jj = np.arange(jj * 3, (jj + 1) * 3)
-        #             atom_jj = positions[index_jj]
-        #             force = force + func_derivative(atom_ii[cc], atom_jj[cc], atom_ii, atom_jj, d_ij_k[ii, jj])
-        #
-        #         force_ixyz[ij] = force
-        #         ij += 1
-        lower_index = np.tril_indices(self.num_atoms)
-        for cc in range(0, 3):
-            test_force = np.zeros([self.num_atoms, self.num_atoms])
-            for ii in range(0, self.num_atoms):
-                index_ii = np.arange(ii * 3, (ii + 1) * 3)
-                atom_ii = positions[index_ii]
-                for jj in range(ii+1, self.num_atoms):
-                    if ii == jj:
-                        continue
-                    index_jj = np.arange(jj * 3, (jj + 1) * 3)
-                    atom_jj = positions[index_jj]
-                    test_force[ii, jj] = func_derivative(atom_ii[cc], atom_jj[cc], atom_ii, atom_jj, d_ij_k[ii, jj])
-            test_force[lower_index] = -test_force.T[lower_index]
-            force_ixyz[cc::3] = np.sum(test_force, axis=1)/2
-        return force_ixyz
-
-    def calc_energy_numerical(self, positions, d_ij_k):
-        def func_distance(atom_i, atom_j):
-            return np.linalg.norm(atom_i-atom_j)
-
-        energy = 0.0
-        for ii in range(0, self.num_atoms):
-            index_ii = np.arange(ii * 3, (ii + 1) * 3)
-            atom_ii = positions[index_ii]
-            for jj in range(ii+1, self.num_atoms): #ii+1
-                # if ii == jj:
-                #     continue
-                index_jj = np.arange(jj * 3, (jj + 1) * 3)
-                atom_jj = positions[index_jj]
-                distance = func_distance(atom_ii, atom_jj)
-                energy = energy + 1./distance**(4./2.) * (d_ij_k[ii, jj]-distance)**2
-        return energy
-
-    def energy_gradient_idpp_fucntion(self, position, d_ij_k):
-        grad = self.calc_forces_numerical(position, d_ij_k)
-        e = self.calc_energy_numerical(position, d_ij_k)
-        return e, grad
 
 
 class Images:
@@ -228,6 +138,13 @@ class Images:
         for element in self.images:
             if all(element.position) == position:
                 self.energy_gradient_func(element)
+
+    def set_energy_gradient(self, energy, gradient):
+        ii = 0
+        for element in self.images:
+            element.energy = energy[ii]
+            element.gradient = gradient[ii, :]
+            ii += 1
 
     def set_energy_gradient_func(self, energy_gradient_func):
         self.energy_gradient_func = energy_gradient_func
@@ -257,6 +174,25 @@ class Images:
         for element in self.images:
             positions.append(np.reshape(element.position, [len(self.atom_list), 3]))
         return positions
+
+    def get_image_energy_list(self):
+        energy = []
+        for element in self.images:
+            energy.append(element.energy)
+        return energy
+
+    def get_image_position_2D_array(self):
+        positions = np.zeros([len(self.images), len(self.images[0].position)])
+        for ii in range(0, len(self.images)):
+            positions[ii,:] = self.images[ii].position
+        return positions
+
+    def get_image_gradient_2Darray(self):
+        force = np.zeros([len(self.images), len(self.images[0].gradient)])
+        for ii in range(0, len(self.images)):
+            force[ii, :] = self.images[ii].gradient
+
+        return force
 
     def update_rot_Mat(self):
         positions, rot_mat = rotation_geometry(self.get_positions())
@@ -306,10 +242,6 @@ class Images:
         else:
             for element in self.images:
                 element.spring_constant = spring_constant
-
-    def add_image(self, index, position, spring_constant):
-        img = Image(position)
-        self.images.insert(index, img)
 
     def number_images_check(self):
         if len(self.images) < 3:
@@ -366,26 +298,11 @@ class Image:
     def set_energy_gradient(self, energy_gradient_function, *args):
         self.energy, self.gradient = energy_gradient_function(self.position, *args)
 
-#
     def get_position(self):
-        return np.reshape(self.position, [len(self.position)/3,3])
+        return self.position
 
-    def get_force(self, position, *args):
-        self.position = position
-        self.images_parent.update_single_image(self, args[0][0], args[0][1])
-
+    def get_force(self, *args):
         if not self.climbing_image:
-            force = np.dot(self.spring_force, self.tangent)*self.tangent - \
-                   self.gradient + np.dot(self.gradient, self.tangent) * self.tangent  # -(gradient - parallel Component)
-        else:
-            force = 2.*np.dot(self.gradient, self.tangent) * self.tangent
-        self.force = force
-        return self.energy, force
-
-    def new_get_force(self, *args):
-        if not self.climbing_image:
-            # print(self.gradient.reshape([8,3]))
-            # print('--------')
             force = np.dot(self.spring_force, self.tangent)*self.tangent - \
                    self.gradient + np.dot(self.gradient, self.tangent) * self.tangent  # -(gradient - parallel Component)
         else:
@@ -465,13 +382,8 @@ class Optimizer:
 
             for ii in range(index_start, index_end):
                 opt_method = images.get_images()[ii].optimizer
-                # energy, grad = images.get_images()[ii].new_get_force()
-                # print(grad)
-                # print(images.get_images()[ii].get_norm_force())
-                # print(images.get_images()[ii].get_para_force())
-                # print('*************************************')
                 images.get_images()[ii].position = opt_method.new_step(images.get_images()[ii].new_get_force, images.get_images()[ii].position)
-
+                print(images.get_images()[ii].get_norm_force())
             if rm_rot_trans:
                 images.update_rot_Mat()
                 for ii in range(index_start, index_end):
@@ -479,7 +391,6 @@ class Optimizer:
             images.update_images(idpp)
 
             if not idpp:
-                # print('*************************************')
                 xyz_writer.write_images2File(images.get_positions(), 'Test_step.xyz', images.atom_list)
                 data_writer.write('Ethane_3_images_'+str(step)+'.xyz', images.get_image_position_list(), images.atom_list)
 
