f74b02378907296b5b591d6fc87060c3a9df623c
kschmuck <klemens.schmuck@tugraz.at>
2018-03-26 09:06:42 +0200

    new example potential

---
+++ mueller_brown_pes.py
@@ -0,0 +1,42 @@
+import numpy as _np
+# müller brown potential energy surface
+# http://demonstrations.wolfram.com/TrajectoriesOnTheMullerBrownPotentialEnergySurface/
+
+
+
+# A, a, b, c, x_zero, y_zero
+coefficients = _np.array([[-200,    -1,     0,      -10,   1,     0],
+                          [-100,    -1,     0,       10,   0,     0.5],
+                          [-170,    -6.5,   11,     -6.5, -0.5,   1.5],
+                          [15,      0.7,    0.6,    0.7,  -1,     1]])
+
+
+def energy(x, y):
+    return sum(coeff[0]*(_np.exp(coeff[1]*(x-coeff[4])**2 + coeff[2]*(x-coeff[4])*(y-coeff[5])
+                         + coeff[3]*(y-coeff[5])**2)) for coeff in coefficients)
+
+
+def energy_list(xy):
+    e = []
+    for element in xy:
+        e.append(energy(element[0], element[1]))
+    return e
+
+
+def gradient(x, y):
+    dx = sum((2 * coeff[1] * (x - coeff[4]) + coeff[3] * (y - coeff[5])) * coeff[0] * (_np.exp(
+        coeff[1] * (x - coeff[4]) ** 2 + coeff[2] * (x - coeff[4]) * (y - coeff[5]) + coeff[3] * (y - coeff[5]) ** 2))
+              for coeff in coefficients)
+
+    dy = sum((2 * coeff[3] * (y - coeff[5]) + coeff[3] * (x - coeff[4])) * coeff[0] * (_np.exp(
+        coeff[1] * (x - coeff[4]) ** 2 + coeff[2] * (x - coeff[4]) * (y - coeff[5]) + coeff[3] * (y - coeff[5]) ** 2))
+              for coeff in coefficients)
+
+    return dx, dy
+
+
+def gradient_list(xy):
+    grad = []
+    for element in xy:
+        grad.append(gradient(element[0], element[1]))
+    return grad
