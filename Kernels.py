bug fix: isotrop Kernel, multipilcation Kernel, theta derivative isotrop Kernel

Kernels.py
@@ -14,6 +14,7 @@ from sklearn.base import clone
 from sklearn.externals.funcsigs import signature
 
 # TODO Exponentiation Class  derivative with respect to x, y
+# TODO Matern Class  derivative with respect to x, y
 # ToDo modify description
 
 def _check_length_scale(X, length_scale):
@@ -658,8 +659,8 @@ class Sum(KernelOperator):
         """
 
         if eval_gradient:
-            K1, K1_gradient = self.k1(X, Y, eval_gradient=True)
-            K2, K2_gradient = self.k2(X, Y, eval_gradient=True)
+            K1, K1_gradient = self.k1(X, Y, dx=dx, dy=dy, eval_gradient=True)
+            K2, K2_gradient = self.k2(X, Y, dx=dx, dy=dy, eval_gradient=True)
 
             if dx != 0 or dy != 0:
                  if isinstance(self.k1, ConstantKernel):
@@ -752,8 +753,8 @@ class Product(KernelOperator):
             is True.
         """
         if eval_gradient:
-            K1, K1_gradient = self.k1(X, Y, eval_gradient=True)
-            K2, K2_gradient = self.k2(X, Y, eval_gradient=True)
+            K1, K1_gradient = self.k1(X, Y, dx=dx, dy=dy, eval_gradient=True)
+            K2, K2_gradient = self.k2(X, Y, dx=dx, dy=dy, eval_gradient=True)
 
             if dx != 0 or dy != 0:
                 if isinstance(self.k1, ConstantKernel):
@@ -763,15 +764,24 @@ class Product(KernelOperator):
                     K2_gradient = np.zeros_like(K2_gradient)
                     K2 = np.zeros_like(K2)
 
+                K1_0, K1_gradient_0 = self.k1(X, Y, eval_gradient=True)
+                K2_0, K2_gradient_0 = self.k2(X, Y, eval_gradient=True)
+
+                return K1*K2_0 + K2 * K1_0, \
+                       np.dstack((K1_gradient * K2_0[:, :, np.newaxis] + K1_gradient_0 * K2[:, :, np.newaxis],
+                                  K2_gradient * K1_0[:, :, np.newaxis]+ K2_gradient_0 * K1[:, :, np.newaxis]))
+
             return K1 * K2, np.dstack((K1_gradient * K2[:, :, np.newaxis], K2_gradient * K1[:, :, np.newaxis]))
         else:
-            K1 = self.k1(X, Y, eval_gradient=False)
-            K2 = self.k2(X, Y, eval_gradient=False)
+            K1 = self.k1(X, Y, dx=dx, dy=dy, eval_gradient=False)
+            K2 = self.k2(X, Y, dx=dx, dy=dy, eval_gradient=False)
             if dx != 0 or dy != 0:
                 if isinstance(self.k1, ConstantKernel):
                     K1 = np.zeros_like(K1)
                 if isinstance(self.k2, ConstantKernel):
                     K2 = np.zeros_like(K2)
+                return K1*self.k2(X, Y, dx=0, dy=0, eval_gradient=False) \
+                       + K2 * self.k1(X, Y, dx=0, dy=0, eval_gradient=False)
             return K1 * K2
 
     def diag(self, X):
@@ -1049,7 +1059,6 @@ class ConstantKernel(StationaryKernelMixin, Kernel):
         return "{0:.3g}**2".format(np.sqrt(self.constant_value))
 
 
-
 class RBF(StationaryKernelMixin, NormalizedKernelMixin, Kernel):
     """Radial-basis function kernel (aka squared-exponential kernel).
 
@@ -1129,43 +1138,41 @@ class RBF(StationaryKernelMixin, NormalizedKernelMixin, Kernel):
         if not self.anisotropic or length_scale.shape[0] == 1:
             if dx != 0 or dy != 0:
                 if dx == dy:
-                    K = K * (1 - np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])**2 / length_scale) / length_scale**2
+                    K = K * (1 - np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])**2 / length_scale**2) / length_scale**2
                 elif dx == 0:
-                    K = -K *np.subtract.outer(X[:, dy-1].T, Y[:, dy-1])/length_scale**2
+                    K = K * np.subtract.outer(X[:, dy-1].T, Y[:, dy-1])/length_scale**2
                 elif dy == 0:
-                    K = K * np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])/length_scale**2
+                    K = -K * np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])/length_scale**2
                 else:
                     K = -K * np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])*np.subtract.outer(X[:, dy-1].T, Y[:, dy-1])\
                                  /length_scale**4
         else:
             if dx != 0 or dy != 0:
                 if dx == dy:
-                    K = K * (1 - np.subtract.outer(X[:,dx-1], Y[:, dx-1])**2/length_scale[dx-1]**2)\
+                    K = K * (1 - np.subtract.outer(X[:,dx-1].T, Y[:, dx-1])**2/length_scale[dx-1]**2)\
                              /length_scale[dx-1]**2
                 elif dx == 0:
-                    K = K * np.subtract.outer(X[:, dy-1], Y[:, dy-1])/length_scale[dy-1]**2
+                    K = K * np.subtract.outer(X[:, dy-1].T, Y[:, dy-1])/length_scale[dy-1]**2
                 elif dy == 0:
-                    K = K * np.subtract.outer(X[:, dx-1], Y[:, dx-1])/length_scale[dx-1]**2
+                    K = -K * np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])/length_scale[dx-1]**2
                 else:
-                    K = - K * np.subtract.outer(X[:, dx-1], Y[:, dx-1])*np.subtract.outer(X[:, dy-1], Y[:, dy-1])\
-                             /(length_scale[dx-1]**2*length_scale[dy-1]**2)
+                    K = - K * np.subtract.outer(X[:, dx-1].T, Y[:, dx-1])*np.subtract.outer(X[:, dy-1].T, Y[:, dy-1])\
+                             / (length_scale[dx-1]**2 * length_scale[dy-1]**2)
 
         if eval_gradient:
             if self.hyperparameter_length_scale.fixed:
                 # Hyperparameter l kept fixed
                 return K, np.empty((X.shape[0], Y.shape[0], 0))
             elif not self.anisotropic or length_scale.shape[0] == 1:
-                if dx == 0 and dy ==0:
+                if dx == 0 and dy == 0:
                     K_gradient = (K * dists)[:, :, np.newaxis]
                 elif (dx != 0 and dy == 0) or (dx == 0 and dy != 0):
-                    K_gradient = (K * (dists - 1))[:, :, np.newaxis]
+                    K_gradient = (K * (dists - 2))[:, :, np.newaxis]
                 else:
                     if dx == dy:
-                        K_gradient = (np.exp(-.5 * dists)*((dists-1)/length_scale**2+(2-dists)
-                                    *np.subtract.outer(X[:, dx-1], Y[:, dx-1])**2/length_scale**2))[:, :, np.newaxis]
+                        K_gradient = (np.exp(-.5 * dists)/length_scale**2*(dists - 2 - dists * (dists - 4)))[:, :, np.newaxis]
                     else:
-                        K_gradient = (K
-                                      * (dists - 2))[:, :, np.newaxis]
+                        K_gradient = (np.exp(-.5 * dists) * dists/length_scale**2 * (4 - dists))[:, :, np.newaxis]
                 return K, K_gradient
             elif self.anisotropic:
 
@@ -1176,7 +1183,7 @@ class RBF(StationaryKernelMixin, NormalizedKernelMixin, Kernel):
 
                 elif (dx != 0 and dy == 0) or (dy != 0 and dx == 0):
                     K_gradient = grad * K[..., np.newaxis]
-                    K_gradient[:, :, dx-1] = K_gradient[:, :, dx-1] - K
+                    K_gradient[:, :, dx-1] = K_gradient[:, :, dx-1] - 2*K
                 else:
                     if dx == dy:
                         K_gradient = grad * K[..., np.newaxis]
@@ -1199,3 +1206,30 @@ class RBF(StationaryKernelMixin, NormalizedKernelMixin, Kernel):
             return "{0}(length_scale={1:.3g})".format(
                 self.__class__.__name__, np.ravel(self.length_scale)[0])
 
+
+
+class OLDRBF:
+    def __init__(self, gamma=1.):
+        self.gamma = gamma
+
+    def __call__(self, x, y, dx=0, dy=0, eval_gradient=0):
+        # dp derivative of the parameters is used for the GPR
+        # in case of GPR the gamma have to be redefined outside to gamma = 1/(2*exp(length scale)) because length scale
+        # is the hyper parameter of interest
+        # mat = _np.tile(_np.sum(x ** 2, axis=1), (len(y), 1)).T + _np.tile(_np.sum(y ** 2, axis=1),
+        #                                                                   (len(x), 1)) - 2 * x.dot(y.T)
+        mat = _spdist.cdist(x/self.gamma, y/self.gamma, 'sqeuclidean')
+        exp_mat = np.exp(-0.5* mat)
+        if dx == dy:
+            if dx == 0:
+                return exp_mat
+            else:
+                return -1.0 /self.gamma * exp_mat * (
+                    1.0 / self.gamma * np.subtract.outer(x[:, dy - 1].T, y[:, dy - 1]) ** 2 - 1)
+        elif dx == 0:
+            return -1.0 / self.gamma * exp_mat * np.subtract.outer(x[:, dy - 1].T, y[:, dy - 1])
+        elif dy == 0:
+            return 1.0 / self.gamma * exp_mat * np.subtract.outer(x[:, dx - 1].T, y[:, dx - 1])
+        else:
+            return -2.0 / self.gamma ** 2 * exp_mat * np.subtract.outer(x[:, dx - 1].T, y[:, dx - 1]) \
+                   * np.subtract.outer(x[:, dy - 1].T, y[:, dy - 1])

MLDerivative.py
@@ -69,7 +69,7 @@ class ML:
                 return self._alpha[self._support_index_alpha].dot(self.kernel(
                     self.x_train[self._support_index_alpha], x)) + sum(
                     self._beta[self._support_index_beta[ii], ii].dot(self.kernel(
-                        self.x_prime_train[self._support_index_beta[ii]], x, dy=ii+1)) for ii in range(self.n_dim)) \
+                        self.x_prime_train[self._support_index_beta[ii]], x, dx=ii+1)) for ii in range(self.n_dim)) \
                        + self._intercept
 
             elif self.n_samples != 0:
@@ -78,7 +78,7 @@ class ML:
 
             else:
                 return sum(self._beta[self._support_index_beta[ii], ii].dot(self.kernel(
-                    self.x_prime_train[self._support_index_beta[ii]], x, dy=ii+1)) for ii in range(self.n_dim))
+                    self.x_prime_train[self._support_index_beta[ii]], x, dx=ii+1)) for ii in range(self.n_dim))
 
         else:
             raise ValueError('not fitted yet')
@@ -94,20 +94,20 @@ class ML:
             if self.n_samples_prime != 0 and self.n_samples != 0:
                 for ii in range(self.n_dim):
                     ret_mat[:, ii] = self._alpha[self._support_index_alpha].dot(self.kernel(
-                        self.x_train[self._support_index_alpha], x, dx=ii+1)) \
+                        self.x_train[self._support_index_alpha], x, dy=ii+1)) \
                                      + sum([self._beta[self._support_index_beta[jj], jj].dot(self.kernel(
-                        self.x_prime_train[self._support_index_beta[jj]], x, dx=ii+1, dy=jj+1)) for jj in
+                        self.x_prime_train[self._support_index_beta[jj]], x, dy=ii+1, dx=jj+1)) for jj in
                         range(self.n_dim)])
 
             elif self.n_samples != 0:
                 for ii in range(self.n_dim):
                     ret_mat[:, ii] = self._alpha[self._support_index_alpha].dot(self.kernel(
-                        self.x_train[self._support_index_alpha], x, dx=ii+1))
+                        self.x_train[self._support_index_alpha], x, dy=ii+1))
 
             else:
                 for ii in range(self.n_dim):
                     ret_mat[:, ii] = sum([self._beta[self._support_index_beta[jj], jj].dot(self.kernel(
-                        self.x_prime_train[self._support_index_beta[jj]], x, dx=ii+1, dy=jj+1)) for jj in
+                        self.x_prime_train[self._support_index_beta[jj]], x, dy=ii+1, dx=jj+1)) for jj in
                         range(self.n_dim)])
             return ret_mat
         else:
@@ -119,7 +119,7 @@ class ML:
         :param x: prediction pattern, for derivative and function value the same shape = [N_samples, N_features]
         :return: function value prediction, derivative prediction
         """
-        x = x.reshape(-1,2)
+        x = x.reshape(-1,self.n_dim)
         return self.predict(x), self.predict_derivative(x).reshape(-1), None
 
 
@@ -550,7 +550,7 @@ class RLS(ML):
         self._support_index_beta = []
         for ii in range(self.n_dim):
             self._support_index_beta.append(np.arange(0, self.n_samples_prime, 1))
-
+        self.debug = kernel_mat
         self._is_fitted = True
 
 
@@ -573,25 +573,13 @@ class GPR(ML):
         ML.__init__(self, kernel)
 
     def get_hyper_parameter(self):
-        if isinstance(self.kernel, Kernels.OLDRBFGrad):
-            return np.log(self.kernel.hyper_parameter)
-        else:
-            return self.kernel.theta
+        return self.kernel.theta
 
     def set_hyper_parameter(self, hyper_parameter):
-        if isinstance(self.kernel, Kernels.OLDRBFGrad):
-            self.kernel.hyper_parameter = np.exp(hyper_parameter)
-        else:
-            self.kernel.theta = hyper_parameter
+        self.kernel.theta = hyper_parameter
 
     def get_bounds(self):
-        if isinstance(self.kernel, Kernels.OLDRBFGrad):
-            bounds = []
-            for element in self.kernel.bounds:
-                bounds.append(element)
-            return np.log(np.array(bounds))
-        else:
-            return self.kernel.bounds
+        return self.kernel.bounds
         #
 
     def fit(self, x_train, y_train, x_prime_train=None, y_prime_train=None, noise=10**-10, restarts=0,
@@ -604,12 +592,14 @@ class GPR(ML):
         else:
             self.y_prime_mean = np.mean(y_prime_train, axis=0)
             self._y_vec = np.concatenate([self.y_train - self.y_mean, self.y_prime_train.flatten()])
+            # self._y_vec = np.concatenate([self.y_train, self.y_prime_train.flatten()])
 
         self.optimize(self.get_hyper_parameter())
         initial_hyper_parameters = self.get_hyper_parameter()
         opt_hyper_parameter = []
         value = []
         opt = self._opt_routine(initial_hyper_parameters, method=fit_method)
+
         opt_hyper_parameter.append(opt[0])
         value.append(opt[1])
 
@@ -704,6 +694,7 @@ class GPR(ML):
         # print(self.get_hyper_parameter())
         if self.n_samples_prime == 0:
             predictive_mean = (self.kernel(x, self.x_train).dot(self._alpha)) + self._intercept
+
             if error_estimate:
                 v = cho_solve((self.L, True), self.kernel(self.x_train, x))
 
@@ -717,11 +708,11 @@ class GPR(ML):
 
         else:
             predictive_mean = self._alpha.dot(self.kernel(self.x_train, x)) + self._intercept \
-                       + sum(self._beta[:, ii].dot(self.kernel(self.x_prime_train, x, dy=ii+1)) for ii in range(self.n_dim))
+                       + sum(self._beta[:, ii].dot(self.kernel(self.x_prime_train, x, dx=ii+1)) for ii in range(self.n_dim))
             if error_estimate:
                 n, d = x.shape
-                k_mat = self.create_mat(self.x_train, x, x1_prime=self.x_prime_train, x2_prime=x)
-                mat = self.create_mat(x, x, x1_prime=x, x2_prime=x)
+                k_mat = create_mat(self.kernel, self.x_train, x, x1_prime=self.x_prime_train, x2_prime=x, dx_max=self.n_dim, dy_max=self.n_dim)
+                mat = create_mat(self.kernel, x, x, x1_prime=x, x2_prime=x, dx_max=self.n_dim, dy_max=self.n_dim)
                 v = cho_solve((self.L, True), k_mat)
                 predictive_covariance = mat - k_mat.T.dot(v)
                 predictive_covariance[predictive_covariance[np.diag_indices(len(predictive_covariance))] < 0.0] = 0.0

SVM_Test.py
@@ -1,98 +1,92 @@
 import NEB as neb
 import numpy as np
-# import optimize as opt
 from pes import gradient, energy, energy_gradient, energy_xy_list, gradient_xy_list, minimum
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-# import SVM as sv
 import MLDerivative as sv
 import sklearn as sk
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection, Patch3DCollection
 from sklearn import svm, gaussian_process, model_selection
 import Kernels
 import copy
+from sklearn import gaussian_process as gpr
 
-offset = 3
-amplitude = 2
-modification = 1/2.
-
-def energy_1D(x):
-    # return np.sinc(x)
-    return amplitude*np.sin(modification*x) + offset
-
-
-def gradient_1D(x):
-    # return np.cos(x)/x+np.sinc(x)/x
-    return modification*amplitude*np.cos(x*modification)
-
-
-gamma = 0.1
-epsilon = 0.01
-c1 = 1000000.
-c2 = 1.
-
-seed = 123
-np.random.seed(seed)
-
-sv_test = []
-# sv_test.append(sv.IRWLS(Kernels.RBF()))
-sv_test.append(sv.RLS(copy.deepcopy(Kernels.RBF())))
-# sv_test.append(sv.GPR(Kernels.newRBFGrad()))
-
-sk_test = svm.SVR(C=c1, kernel='rbf', gamma=gamma, epsilon=epsilon)
-
-# sk_test_2 = gaussian_process.GaussianProcessRegressor()
-x_predict = np.linspace(-15*np.pi, 15*np.pi, 2000).reshape(-1,1)
-x = (np.random.rand(5) - 0.5) * np.pi * 18
-x = x.reshape(-1, 1)
-
-Gamma = np.linspace(0.01,1.,10)
-fig = plt.figure()
-for element in Gamma:
-    # print(sk_test.get_params(deep=True))
-    sk_test.gamma = element
-    scores = model_selection.cross_val_score(sv.RLS(copy.deepcopy(Kernels.RBF(gamma = element))), x, energy_1D(x).reshape(-1), cv=3)
-    sk_test.fit(x, energy_1D(x).reshape(-1))
-    y_predicted = sk_test.predict(x_predict.reshape(-1,1))
-    string = 'gamma = ' + str(element) + '  score sum = ' + str(np.sum(scores))
-    plt.plot(x_predict, y_predicted, label=string)
-    # print(np.max(scores))
-    # print(sk_test.get_params(deep=True))
-plt.plot(x_predict, energy_1D(x_predict), ls='--', color='k')
-plt.plot(x, energy_1D(x), ls='None', color='k', marker='o', label='Training Points', alpha=0.4)
-
-plt.legend()
-plt.show()
-
+# ### 1D Test case
+# offset = 3
+# amplitude = 2
+# modification = 1/2.
+#
+# def energy_1D(x):
+#     # return np.sinc(x)
+#     return amplitude*np.sin(modification*x) + offset
+#
+#
+# def gradient_1D(x):
+#     # return np.cos(x)/x+np.sinc(x)/x
+#     return modification*amplitude*np.cos(x*modification)
+#
+#
+# gamma = 0.1
+# epsilon = 0.01
+# c1 = 1.
+# c2 = 1.
+#
+# seed = 123
+# np.random.seed(seed)
+#
+# sv_test = []
+# kernel_gpr = Kernels.RBF()*Kernels.ConstantKernel()
+# kernel_rls = Kernels.RBF()
+#
+# # sv_test.append(sv.RLS(kernel_rls))
+# sv_test.append(sv.GPR(kernel_gpr))
+#
+# sk_test = svm.SVR(C=c1, kernel='rbf', gamma=gamma, epsilon=epsilon)
+# sk_test_gpr = gpr.GaussianProcessRegressor(gpr.kernels.RBF()*gpr.kernels.ConstantKernel(), normalize_y=True)
+#
+# x_predict = np.linspace(-15*np.pi, 15*np.pi, 2000).reshape(-1,1)
+# x = (np.random.rand(30) - 0.5) * np.pi * 18
+# x = x.reshape(-1, 1)
 #
 # sk_test.fit(x, energy_1D(x).reshape(-1))
+# sk_test_gpr.fit(x, energy_1D(x).reshape(-1))
 # np.random.seed()
 # sv_val = []
-# sk_val = sk_test.predict(x_predict)
-# sv_grad_val = []
-# sk_test_2.kernel = gaussian_process.kernels.ConstantKernel(1.)*gaussian_process.kernels.RBF(1.) + gaussian_process.kernels.ConstantKernel(np.mean(energy_1D(x)))
-# sk_test_2.normalize_y = True
-# sk_test_2.fit(x, energy_1D(x).reshape(-1), )
-# sk_val, sk_std = sk_test_2.predict(x_predict, return_cov=True)
-# mat = []
-# for element in sv_test:
-#     element.fit(x, energy_1D(x).reshape(-1))#, noise=10**-10) # np.zeros([0,1]), np.zeros(0), x_prime=x, y_prime=gradient_1D(x).reshape(-1)
-#     # element.fit(x, energy_1D(x).reshape(-1), x_prime_train=x, y_prime_train=gradient_1D(x).reshape(-1, 1))#10**3)#), C1=cc, C2=vv)#, epsilon=ii, C1=cc, C2=vv, eps=10**-6, max_iter=10**4, error_cap=10**-8)
-#     # element.fit(np.zeros([0,1]), np.zeros(0), x_prime=x, y_prime=gradient_1D(x).reshape(-1), C1=cc, C2=vv)
-#     sv_val.append(element.predict(x_predict))
 #
-# color = ['g', 'b', 'r']
+# # sk_val = [(sk_test.__class__.__name__, sk_test.predict(x_predict)),
+# #           (sk_test_gpr.__class__.__name__, sk_test_gpr.predict(x_predict))]
+# sk_val = [(sk_test_gpr.__class__.__name__, sk_test_gpr.predict(x_predict, return_std=True))]
+#
+# for element in sv_test:
+#     # element.fit(x, energy_1D(x).reshape(-1))
+#     element.fit(x, energy_1D(x).reshape(-1), x_prime_train=x, y_prime_train=gradient_1D(x).reshape(-1, 1))
+#     # element.fit(np.zeros([0,1]), np.zeros(0), x_prime=x, y_prime=gradient_1D(x).reshape(-1))
+#     # sv_val.append((element.__class__.__name__, element.predict(x_predict)))
+#
+#     sv_val.append((element.__class__.__name__, element.predict(x_predict, error_estimate=True)))
+#
 # fig = plt.figure()
 # plt.plot(x_predict, energy_1D(x_predict), ls='--', color='k', label='True Path')
-# plt.plot(x, energy_1D(x), ls='None', color='k', marker='o', label='Training Points', alpha=0.4)
-# plt.fill_between(x_predict.reshape(-1), sk_val.reshape(-1) + 2. * np.sqrt(np.diag(sk_std)), sk_val.reshape(-1) - 2. * np.sqrt(np.diag(sk_std)), facecolor=[0.7539, 0.62890625, 0.89453125, .5],
-#                  linewidths=0.0)
+# plt.plot(x, energy_1D(x), ls='None', color='k', marker='x', label='Training Points')
 #
-# for jj in range(len(sv_test)):
-#     plt.plot(x_predict, sv_val[jj], color=color[jj], label=sv_test[jj].__class__.__name__)
-# plt.plot(x_predict, sk_val, color='y', label='scikit', ls='--')
+# for element in sv_val:
+#     plt.plot(x_predict, element[1][0], label=element[0])
+#     plt.fill_between(x_predict.reshape(-1), element[1][0] + 2. * np.sqrt(element[1][1]),
+#                      element[1][0] - 2. * np.sqrt(element[1][1]), facecolor=[0.7539, 0.62890625, 0.89453125, .5],
+#                      linewidths=0.0, label='gpr')
+#
+#
+# for element in sk_val:
+#     string = element[0] + 'scikit'
+#     plt.plot(x_predict, element[1][0], label=string, ls='--')
+#     plt.fill_between(x_predict.reshape(-1), element[1][0] + 2. * np.sqrt(element[1][1]),
+#                      element[1][0] - 2. * np.sqrt(element[1][1]), facecolor=[0.62890625, 0.89453125, 0.7539, .5],
+#                      linewidths=0.0, label='scikit')
 # plt.legend()
 # plt.show()
+# #
+# # # plt.fill_between(x_predict.reshape(-1), sk_val.reshape(-1) + 2. * np.sqrt(np.diag(sk_std)), sk_val.reshape(-1) - 2. * np.sqrt(np.diag(sk_std)), facecolor=[0.7539, 0.62890625, 0.89453125, .5],
+# # #                  linewidths=0.0)
 
 #######################################################################################################################
 # #######################################################################################################################
@@ -100,7 +94,7 @@ plt.show()
 n = 3
 grid = 3.
 # xx, yy = np.meshgrid(np.linspace(-grid, grid, n), np.linspace(-grid, grid, n))
-xy = np.random.uniform(size=(10, 2), low = -5., high = 5.)
+xy = np.random.uniform(size=(10, 2), low=-5., high=5.)
 xx = xy[:,0]
 yy = xy[:,1]
 
@@ -117,42 +111,41 @@ xy_pred = np.concatenate([xx_pred.reshape(-1,1), yy_pred.reshape(-1,1)], axis=1)
 #
 # images = neb.create_images(minima_a, minima_b, 5)
 # images = neb.ImageSet(images)
-# # images.energy_gradient_func = energy_gradient
-# images.set_spring_constant(1)
-# # images.update_images('simple_improved')
-#
 # start_pos = images.get_image_position_2D_array()
+#
 # xy = start_pos
 # xx = xy[:,0]
 # yy = xy[:,1]
 
-c1 = 10000.
-c2 = 10000.
+c1 = 1.
+c2 = 1.
 gamma = 1.1
 epsilon = 0.01
 
 sv_val_2d = []
 sv_test_2d = []
+sk_test_2d = []
+sk_val_2d = []
 
-sv_test_2d.append(sv.GPR(Kernels.newRBFGrad()))
+kernel_2d_gpr = Kernels.RBF([1., 1.])*Kernels.ConstantKernel()
+sv_test_2d.append(sv.GPR(Kernels.ConstantKernel()*kernel_2d_gpr))
 sv_test_2d.append(sv.RLS(Kernels.RBF()))
-sk_test_2d = svm.SVR(kernel='rbf', gamma=gamma, epsilon=epsilon)
+
+sk_test_2d.append(gpr.GaussianProcessRegressor(gpr.kernels.RBF([1., 1.]) * gpr.kernels.ConstantKernel(), normalize_y=True))
+sk_test_2d.append(svm.SVR(kernel='rbf', gamma=gamma, epsilon=epsilon))
 ##
-sk_test_2d.fit(xy, energy(xx, yy).reshape(-1))
+for element in sk_test_2d:
+    element.fit(xy, energy(xx, yy).reshape(-1))
+    sk_val_2d.append((element.__class__.__name__, element.predict(xy_pred)))
+
 grad_x, grad_y = gradient(xx, yy)
 grad = np.concatenate([grad_x.reshape(-1,1), grad_y.reshape(-1, 1)], axis=1)
 
 for element in sv_test_2d:
     # element.fit(xy, energy(xx, yy).reshape(-1))  # np.zeros([0,2]), np.zeros(0), x_prime=xy, y_prime=grad
-    element.fit(xy, energy(xx, yy).reshape(-1), x_prime_train=xy,  y_prime_train=grad) # np.zeros([0,2]), np.zeros(0), x_prime=xy, y_prime=grad
-    val = element.predict(xy_pred) #  covar, var
+    element.fit(xy, energy(xx, yy).reshape(-1), x_prime_train=xy,  y_prime_train=grad)
+    sv_val_2d.append((element.__class__.__name__, element.predict(xy_pred)))
 
-    sv_val_2d.append(val.reshape(-1, n_pred))
-    # element.fit(xy, energy(xx, yy).reshape(-1), x_prime_train=xy,  y_prime_train=grad, C1=c1, C2=c2) # np.zeros([0,2]), np.zeros(0), x_prime=xy, y_prime=grad
-    # element.fit(xy, energy(xx, yy).reshape(-1), C1=c1,C2=c2)  # np.zeros([0,2]), np.zeros(0), x_prime=xy, y_prime=grad
-    # sv_val_2d.append(element.predict(xy_pred).reshape(-1,n_pred))
-
-sk_val_2d = sk_test_2d.predict(xy_pred)
 
 def fixZorderFactory_scatter(zorder_in):
     class FixZorderScatter(Patch3DCollection):
@@ -193,84 +186,63 @@ def fixZorderFactory_wireframe(zorder_in):
 alpha = 0.5
 rot_angle = 50. #rot_angle = 45.
 azim = -65.#azim = -65.
-fig_2d = plt.figure()
-ax_2d_a = fig_2d.add_subplot(221, projection='3d')
 upper_limit = 3.
 lower_limit = -1.5
 
-ax_2d_a.plot_surface(xx_pred, yy_pred, energy(xx_pred, yy_pred), label='True')
-ax_2d_a.collections[-1].__class__ = fixZorderFactory_surface(0)
-ax_2d_a.plot_wireframe(xx_pred, yy_pred, sk_val_2d.reshape(-1,n_pred), label='scikit', color='y', alpha=alpha)
-ax_2d_a.collections[-1].__class__ = fixZorderFactory_wireframe(1)
-ax_2d_a.scatter(xx, yy, energy(xx, yy), color='k')
-ax_2d_a.collections[-1].__class__ = fixZorderFactory_scatter(5)
-ax_2d_a.elev = rot_angle
-ax_2d_a.azim = azim
-plt.title('scikit')
-# ax_2d_a.scatter(xx, yy, energy(xx, yy), color='k')
 
-ax_2d_b = fig_2d.add_subplot(222, projection='3d')
-ax_2d_b.plot_surface(xx_pred, yy_pred, energy(xx_pred, yy_pred), label='True')
-ax_2d_b.collections[-1].__class__ = fixZorderFactory_surface(0)
-ax_2d_b.plot_wireframe(xx_pred, yy_pred, sv_val_2d[0], label='GPR',  color='y', alpha=alpha)
-ax_2d_b.collections[-1].__class__ = fixZorderFactory_wireframe(1)
-ax_2d_b.scatter(xx, yy, energy(xx, yy), color='k')
-ax_2d_b.collections[-1].__class__ = fixZorderFactory_scatter(5)
-ax_2d_b.elev = rot_angle
-ax_2d_b.azim = azim
-plt.title('GPR')
-# ax_2d_b.scatter(xx, yy, energy(xx, yy), color='k')
-
-ax_2d_c = fig_2d.add_subplot(223, projection='3d')
-ax_2d_c.plot_surface(xx_pred, yy_pred, energy(xx_pred, yy_pred), label='True')
-ax_2d_c.collections[-1].__class__ = fixZorderFactory_surface(0)
-ax_2d_c.plot_wireframe(xx_pred, yy_pred, sv_val_2d[1], label='RLS',  color='y', alpha=alpha)
-ax_2d_c.collections[-1].__class__ = fixZorderFactory_wireframe(1)
-ax_2d_c.scatter(xx, yy, energy(xx, yy), color='k')
-ax_2d_c.collections[-1].__class__ = fixZorderFactory_scatter(5)
-ax_2d_c.elev = rot_angle
-ax_2d_c.azim = azim
-plt.title('RLS')
-#
-# ax_2d_d = fig_2d.add_subplot(224, projection='3d')
-# ax_2d_d.plot_surface(xx_pred, yy_pred, energy(xx_pred, yy_pred), label='True')
-# ax_2d_d.collections[-1].__class__ = fixZorderFactory_surface(0)
-# ax_2d_d.plot_wireframe(xx_pred, yy_pred, sv_val_2d[2], label=method,  color='y', alpha=alpha)
-# ax_2d_d.collections[-1].__class__ = fixZorderFactory_wireframe(1)
-# ax_2d_d.scatter(xx, yy, energy(xx, yy), color='k')
-# ax_2d_d.collections[-1].__class__ = fixZorderFactory_scatter(5)
-# ax_2d_d.elev = rot_angle
-# ax_2d_d.azim = azim
-# plt.title(method[2])
-
-# plt.show()
+def plot_3D(axis, x_plot, y_plot, z_plot, true_function, x_fit, y_fit, title):
+    axis.plot_surface(x_plot, y_plot, true_function(x_plot, y_plot))
+    axis.collections[-1].__class__ = fixZorderFactory_surface(0)
+    axis.plot_wireframe(x_plot, y_plot, z_plot, color='y', alpha=alpha)
+    axis.collections[-1].__class__ = fixZorderFactory_wireframe(1)
+    axis.scatter(x_fit, y_fit, energy(x_fit, y_fit), color='k')
+    axis.collections[-1].__class__ = fixZorderFactory_scatter(5)
+    axis.elev = rot_angle
+    axis.azim = azim
+    axis.set_title(title)
 
 
-fig_2d_contour = plt.figure()
-levels = 50
+def plot_2D(axis, x_plot, y_plot, z_plot, x_fit, y_fit, levels, title):
+    axis.contourf(x_plot, y_plot, z_plot, levels=levels)
+    axis.plot(x_fit, y_fit, ls='None', marker='x', color='k', label='Fitting Points')
+    axis.set_title(title)
+    axis.legend()
 
-ax_contour_a = fig_2d_contour.add_subplot(221)
-a = ax_contour_a.contourf(xx_pred, yy_pred, sk_val_2d.reshape(-1,n_pred), levels = np.linspace(lower_limit, upper_limit, levels))
-fig_2d_contour.colorbar(a, ax=ax_contour_a)
-# ax_contour_a.scatter(xx, yy, marker= 'o', color='k')
-plt.title('scikit')
 
-ax_contour_b = fig_2d_contour.add_subplot(222)
-b = ax_contour_b.contourf(xx_pred, yy_pred, sv_val_2d[0], levels=np.linspace(lower_limit, upper_limit, levels))
-fig_2d_contour.colorbar(b, ax=ax_contour_b)
-# ax_contour_b.scatter(xx, yy, marker='o', color='k')
-plt.title('GPR')
+fig = plt.figure()
 
-ax_contour_c = fig_2d_contour.add_subplot(224)
-c= ax_contour_c.contourf(xx_pred, yy_pred, energy(xx_pred, yy_pred), levels=np.linspace(lower_limit, upper_limit, levels))
-fig_2d_contour.colorbar(c, ax=ax_contour_c)
-# ax_contour_c.scatter(xx, yy, marker='o', color='k')
-plt.title('True')
-#
-ax_contour_d = fig_2d_contour.add_subplot(223)
-d = ax_contour_d.contourf(xx_pred, yy_pred, sv_val_2d[1], levels=np.linspace(lower_limit, upper_limit, levels))
-fig_2d_contour.colorbar(d, ax=ax_contour_d)
-# ax_contour_d.scatter(xx, yy, marker='o', color='k')
-plt.title('RLS')
+plot_3D(fig.add_subplot(221, projection='3d'), xx_pred, yy_pred, sk_val_2d[0][1].reshape(-1, n_pred), energy, xx, yy, sk_val_2d[0][0])
+plot_3D(fig.add_subplot(222, projection='3d'), xx_pred, yy_pred, sk_val_2d[1][1].reshape(-1, n_pred), energy, xx, yy, sk_val_2d[1][0])
+plot_3D(fig.add_subplot(223, projection='3d'), xx_pred, yy_pred, sv_val_2d[0][1].reshape(-1, n_pred), energy, xx, yy, sv_val_2d[0][0])
+plot_3D(fig.add_subplot(224, projection='3d'), xx_pred, yy_pred, sv_val_2d[1][1].reshape(-1, n_pred), energy, xx, yy, sv_val_2d[1][0])
+
+lvl = np.linspace(lower_limit, upper_limit, 50)
+fig_2D = plt.figure()
+plot_2D(fig_2D.add_subplot(231), xx_pred, yy_pred, sk_val_2d[0][1].reshape(-1, n_pred), xx, yy, lvl, sk_val_2d[0][0])
+plot_2D(fig_2D.add_subplot(232), xx_pred, yy_pred, sk_val_2d[1][1].reshape(-1, n_pred), xx, yy, lvl, sk_val_2d[1][0])
+plot_2D(fig_2D.add_subplot(233), xx_pred, yy_pred, sv_val_2d[0][1].reshape(-1, n_pred), xx, yy, lvl, sv_val_2d[0][0])
+plot_2D(fig_2D.add_subplot(234), xx_pred, yy_pred, sv_val_2d[1][1].reshape(-1, n_pred), xx, yy, lvl, sv_val_2d[1][0])
+plot_2D(fig_2D.add_subplot(235), xx_pred, yy_pred, energy(xx_pred, yy_pred), xx, yy, lvl, 'True')
+
 
 plt.show()
+
+
+
+# Gamma = np.linspace(0.01,1.,10)
+# fig = plt.figure()
+# for element in Gamma:
+#     # print(sk_test.get_params(deep=True))
+#     sk_test.gamma = element
+#     scores = model_selection.cross_val_score(sv.RLS(copy.deepcopy(Kernels.RBF(gamma = element))), x, energy_1D(x).reshape(-1), cv=3)
+#     sk_test.fit(x, energy_1D(x).reshape(-1))
+#     y_predicted = sk_test.predict(x_predict.reshape(-1,1))
+#     string = 'gamma = ' + str(element) + '  score sum = ' + str(np.sum(scores))
+#     plt.plot(x_predict, y_predicted, label=string)
+#     # print(np.max(scores))
+#     # print(sk_test.get_params(deep=True))
+# plt.plot(x_predict, energy_1D(x_predict), ls='--', color='k')
+# plt.plot(x, energy_1D(x), ls='None', color='k', marker='o', label='Training Points', alpha=0.4)
+#
+# plt.legend()
+# plt.show()