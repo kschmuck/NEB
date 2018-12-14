1935d397b0acd05e30b20a89ad9094392c207559
kschmuck <klemens.schmuck@tugraz.at>
2018-12-14 11:37:59 +0100

    adding energy to .xyz files, use of IDPP potential

--- ML_Test_File.py
+++ ML_Test_File.py
@@ -1,14 +1,17 @@
 import numpy as np
 import os, sys
-import ethane_PES, ammonia_PES, Pt4C3H8_PES #, AuO2_PES
-import NEB, optimize, data_reader_writer, Kernels, MLDerivative, copy, IDPP
+import ethane_PES, ammonia_PES, Pt4C1H4_PES #, Pt4C3H8_PES, AuO2_PES
+import NEB, optimize, data_reader_writer, Kernels, MLDerivative, copy
 import time
 import warnings
-
+import IDPP as idpp
 
 t = time.clock()
 #
 molecule = 'Ammonia'
+# molecule = 'Ethane'
+# molecule = 'Pt4C3H8'
+molecule = 'Pt4C1H4'
 file_path = os.path.dirname(__file__)
 result_path = os.path.join(file_path, 'Results', molecule)
 sys.stdout.flush()
@@ -16,31 +19,44 @@ sys.stdout.flush()
 # NEB parameters
 n_imgs = 7
 trust_radius = 0.05
-kernel = Kernels.RBF([0.7])
 k = 1e-4
-max_force = 0.001
-max_iter = 1000
-C1 = 1e10
-C2 = 1e10
-max_steps = 21
-# neb_method = 'simple_improved'
-neb_method = 'improved'
+
+max_displacement = None
+
+max_force = 2e-3
+max_iter = 400
+max_steps = 30
+
+if molecule == 'Ammonia':
+    neb_method = 'improved'
+    max_force = 1e-3
+elif molecule == 'Ethane':
+    neb_method = 'simple_improved'
+    max_force = 2e-3
+elif molecule == 'Pt4C1H4':
+    neb_method = 'improved'
+    max_force = 2e-2
+calc_idpp = True
 
 # optimizer
 delta_t = 3.5
-
 opt_fire = NEB.Fire(delta_t, 2*delta_t, trust_radius)
 opt = NEB.Optimizer()
+
+kernel = Kernels.RBF([0.8])
+C1 = 1e10
+C2 = 1e10
+
 
 eps = 1e-5
 restarts = 5
 opt_steps = 1
 optimize_parameters = True
-norm_y = False
-# ml_method = MLDerivative.IRWLS(kernel, C1=C1, C2=C2, epsilon=5*1e-5, epsilon_prime=5*1e-5, max_iter=1e4)
-# ml_method = MLDerivative.RLS(kernel,C1=C1, C2=C2)
+norm_y = True
+# ml_method = MLDerivative.IRWLS(kernel, C1=C1, C2=C2, epsilon=1e-5, epsilon_prime=1e-5, max_iter=1e4)
+# ml_method = MLDerivative.RLS(kernel, C1=C1, C2=C2)
 ml_method = MLDerivative.GPR(kernel, opt_restarts=restarts, opt_parameter=optimize_parameters, noise_value = 1./C1,
-                             noise_derivative=1./C2,  normalize_y=norm_y)
+                              noise_derivative=1./C2,  normalize_y=norm_y)
 
 
 # reading minima and creating imagesAu_O2
@@ -63,34 +79,39 @@ elif molecule == 'Ethane':
     energy_gradient = ethane_PES.energy_and_gradient
 elif molecule == 'Pt4C3H8':
     energy_gradient = Pt4C3H8_PES.energy_and_gradient
-elif molecule == 'Au_O2':
+elif molecule == 'Pt4C1H4':
+    energy_gradient = Pt4C1H4_PES.energy_and_gradient
 
-    k_idpp = 10 ** -4  # 10**-10 #10**-6
-    delta_t_fire = 1.5  # 3.5
-    delta_t_verlete = 0.4  # 0.2
-    force_max = 0.001
-    max_steps = 500
-    epsilon = 0.01  # 00001
-    trust_radius = 0.05  # .1
-    # # --------------------
 
+if calc_idpp:
+    k_idpp = 1e-4
+    delta_t_fire_idpp= 3.5
+    force_max_idpp = max_force*10
+    max_steps_idpp = 1000
+    trust_radius_idpp = 0.01
+    neb_method_idpp = neb_method
+
+    print("molecule idpp = " + molecule + "\n k = %f \n opt_method = Fire \n delta_t_fire = %f \n force_max = %f \n"
+                                          " max_steps = %d \n trust_radius = %f \n tangent_method = %s") \
+         % (k_idpp, delta_t_fire_idpp, force_max_idpp, max_steps_idpp, trust_radius_idpp, neb_method_idpp)
     images.set_spring_constant(k_idpp)
-    opt_fire = NEB.Optimizer.FireNeb(delta_t_fire, 2*delta_t_fire, trust_radius)
-
-    idpp_potential = IDPP.IDPP(images)
+    opt_fire = NEB.Optimizer.FireNeb(delta_t_fire_idpp, 2 * delta_t_fire_idpp, trust_radius_idpp)
+    idpp_potential = idpp.IDPP(images)
     images.energy_gradient_func = idpp_potential.energy_gradient_idpp_function
     opt = NEB.Optimizer()
-    writer.write(os.path.join(result_path, 'ML_Data', 'start_structure.xyz'), images.get_positions(),
-                 atoms)
-    images = opt.run_opt(images, opt_fire, max_steps=max_steps, force_max=force_max, rm_rot_trans=False)
-    writer.write(os.path.join(result_path, 'ML_Data', molecule + 'idpp_structure.xyz'), images.get_positions(),
-                 atoms)
-    energy_gradient = AuO2_PES.energy_and_gradient
-#
+    images = opt.run_opt(images, opt_fire, max_steps=max_steps_idpp, force_max=force_max_idpp, rm_rot_trans=False, freezing=0, opt_minima=False, max_displacement=max_displacement)
+    writer.write(os.path.join(result_path, molecule + '_idpp_structure.xyz'), images.get_positions(), atoms)
 
 images.set_spring_constant(k)
 images.energy_gradient_func = energy_gradient
+
+for img in images:
+    img.frozen = False
+
 images.update_images(neb_method)
+
+images[0].frozen = True
+images[-1].frozen = True
 
 pos = images.get_image_position_2D_array()
 grad = images.get_image_gradient_2Darray()
@@ -151,15 +172,15 @@ while not converged:
         num_beta = sum(len(ii) for ii in ml_method_copy._support_index_beta)
         num_deriv = sum(len(ii) for ii in y_prime_train)
         print('function values:  %d number of support vectors, %d number of non support vectors') % (
-        len(ml_method_copy._support_index_alpha), len(y_train) - len(ml_method_copy._support_index_alpha))
+            len(ml_method_copy._support_index_alpha), len(y_train) - len(ml_method_copy._support_index_alpha))
         print('derivatives values: %d number of support vectors, %d number of non support vectors') % (num_beta,
                                                                                                        num_deriv - num_beta)
 
     print('start neb')
     calc_images.energy_gradient_func = ml_method_copy.predict_val_der
     # force_max = 1e-3
-    calc_images = opt.run_opt(calc_images, copy.deepcopy(opt_fire), force_max=max_force, max_steps=max_iter, freezing=0, tangent_method=neb_method, opt_minima=False)
-    writer.write(os.path.join(result_path, 'ML_Data', 'Step_'+str(step)+ '_structure.xyz'), calc_images.get_positions(), atoms)
+    calc_images = opt.run_opt(calc_images, copy.deepcopy(opt_fire), force_max=max_force, max_steps=max_iter, freezing=0, tangent_method=neb_method, opt_minima=False, max_displacement=max_displacement)
+    writer.write(os.path.join(result_path, 'ML_Data', 'Step_'+str(step)+ '_structure.xyz'), calc_images.get_positions(), atoms, energy=images.get_image_energy_list())
     print('---- predicted forces ----')
     uu = 0
     for element in calc_images:
@@ -192,7 +213,7 @@ while not converged:
         opt.fmax = max_force
         if opt.is_converged(calc_images[1:-1]):
             print('solution found')
-            writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms)
+            writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms, energy=images.get_image_energy_list())
             converged = True
     else:
         # pos = calc_images.get_image_position_2D_array()
@@ -210,12 +231,13 @@ while not converged:
         converged = True
         print('no solution obtained')
 
-max_steps = 31
 old_step = step
 step = 0
 calc_images.set_climbing_image()
 images = copy.deepcopy(calc_images)
 max_force *= 0.5
+
+ml_method.kernel = ml_method_copy.kernel
 # trust_radius = trust_radius*.5
 converged = False
 print('#####################################\n #### '+ molecule +' '+ml_method.__class__.__name__ +' _climbing image #### \n #####################################')
@@ -233,13 +255,14 @@ while not converged:
 
     print('start fitting')
     if ml_method.__class__.__name__ == 'GPR':
-        if step < 1:
+        if step < 0:
             ml_method_copy.opt_restarts = 5
             ml_method_copy.opt_parameter = True
         else:
             ml_method_copy.opt_parameter = False
             ml_method_copy.opt_restarts = 0
         ml_method_copy.fit(x_train, y_train, x_prime_train=x_train, y_prime_train=y_prime_train)
+
         print('length_scale = ' + str(ml_method_copy.kernel))
     elif ml_method.__class__.__name__ == 'RLS':
         ml_method_copy.fit(x_train, y_train, x_prime_train=x_train, y_prime_train=y_prime_train)
@@ -249,15 +272,15 @@ while not converged:
         num_beta = sum(len(ii) for ii in ml_method_copy._support_index_beta)
         num_deriv = sum(len(ii) for ii in y_prime_train)
         print('function values:  %d number of support vectors, %d number of non support vectors') % (
-        len(ml_method_copy._support_index_alpha), len(y_train) - len(ml_method_copy._support_index_alpha))
+            len(ml_method_copy._support_index_alpha), len(y_train) - len(ml_method_copy._support_index_alpha))
         print('derivatives values: %d number of support vectors, %d number of non support vectors') % (num_beta,
                                                                                                        num_deriv - num_beta)
     print('start neb')
     calc_images.energy_gradient_func = ml_method_copy.predict_val_der
-    opt.run_opt(calc_images, copy.deepcopy(opt_fire), force_max=max_force, max_steps=max_iter, freezing=0, tangent_method=neb_method)
+    opt.run_opt(calc_images, copy.deepcopy(opt_fire), force_max=max_force, max_steps=max_iter, freezing=0, tangent_method=neb_method, max_displacement=max_displacement)
 
     writer.write(os.path.join(result_path, 'ML_Data', 'Climbing_Step_' + str(step) + '_structure.xyz'),
-                 calc_images.get_positions(), atoms)
+                 calc_images.get_positions(), atoms, energy=images.get_image_energy_list())
     print('---- predicted forces ----')
     uu = 0
     for element in calc_images:
@@ -284,7 +307,7 @@ while not converged:
         if opt.is_converged(calc_images[1:-1]):
             print('solution found')
             # writer = data_reader_writer.Writer()
-            writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms)
+            writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms, energy=images.get_image_energy_list())
             converged = True
         train_list[0].append(pos)
         train_list[1].append(energy)
@@ -300,5 +323,5 @@ while not converged:
     if step >= max_steps:
         converged = True
         print('no solution obtained')
-writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms)
+writer.write(os.path.join(result_path, 'ML_Data', 'end_structure.xyz'), calc_images.get_positions(), atoms, energy=images.get_image_energy_list())
 print('used time ' + str(t-time.clock()))
